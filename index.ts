cf4543e3d30c7a4d4224d53be2b212a28b306c1d
nero <talantsev@medesk.md>
2020-02-24 23:56:19 +0300

    refactor index

--- index.ts
+++ index.ts
@@ -3,16 +3,12 @@ import * as body from "body-parser"
 import Router from "./src/router"
 import env from "./src/cli"
 
-const app = express()
-
-app.use(
-	body.urlencoded({
-		extended: false,
-	}),
-)
-
-app.use(body.json())
-app.use(Router)
-app.listen(env.Port, () => console.log("server started at http://localhost:" + env.Port))
-
-export default app
+export default express()
+	.use(
+		body.urlencoded({
+			extended: false,
+		}),
+	)
+	.use(body.json())
+	.use(Router)
+	.listen(env.Port, () => console.log("server started at http://localhost:" + env.Port))
